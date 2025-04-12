89806fc46a57560010ce8671902bbce51014297d
Soham Rajam <143082072+soham3002@users.noreply.github.com>
2025-04-12 19:46:57 +0530

    Delete backend directory

--- backend/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const reviewController = require('../controllers/reviewController');
-const authMiddleware = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.post('/add', authMiddleware, reviewController.addReview);
-router.get('/:productId', reviewController.getReviews);
-
-module.exports = router;
